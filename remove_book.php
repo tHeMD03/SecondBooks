a59f2e9d2ae657c5ac674153b648091e3ac992d5
Meet Dabhi <md4meetdabhi@gmail.com>
2020-11-18 11:15:24 +0530

    Icons Added to delete items

--- remove_book.php
+++ remove_book.php
@@ -16,9 +16,9 @@ $bid = $_REQUEST['bid'];
 $sql = "DELETE FROM `books details` WHERE book_id='$bid';";
 
 if (mysqli_query($conn, $sql)) {
-	if (file_exists("upload/" . $bid . ".jpg"))
+	if (file_exists("upload/books" . $bid . ".jpg"))
     {
-    	unlink( "upload/". $bid .".jpg" );
+    	unlink( "upload/books". $bid .".jpg" );
     }
     header("Location: books_data.php");
 }
